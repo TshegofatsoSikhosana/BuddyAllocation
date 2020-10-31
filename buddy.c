1509e54b30ad82fddc88fe350f981b4a37083ebb
TshegofatsoSikhosana <ts98sikhosana@gmail.com>
2020-10-31 14:44:39 +0200

    Fixed buddy algorithm

--- buddy.c
+++ buddy.c
@@ -1,141 +1,138 @@
 
 #include "buddy.h"
 #include <sys/mman.h>
-#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <math.h>
 #include <assert.h>
+#include <stdbool.h>
 
-
-#define MIN 5
-#define MAX 12
+#define MINIMUM 5
+#define MAXIMUM 12
 
 struct head {
-  struct head *next;
+
+	int level;
+	struct head *next;
 	struct head *prev;
 	bool status;
-	int level;
+
 };
 
 struct head *new() {
 
-  size_t size = (size_t)pow(2,13);
-	struct head * region = mmap(
-	(void*) size,   // Map from the start of the 2^12th page
-	size,                         // for one page length
-	PROT_READ|PROT_WRITE|PROT_EXEC,
-	MAP_ANON|MAP_PRIVATE,             // to a private block of hardware memory
-	0,
-	0
-	);
 
-	if (region == MAP_FAILED) {
-		perror("Could not mmap");
+	size_t 	pagesize = (size_t)pow(2, MAXIMUM);
+	struct head* map = mmap(
+	(void *) pagesize,
+	pagesize,
+	PROT_READ|PROT_WRITE|PROT_EXEC,
+	MAP_ANON|MAP_PRIVATE,
+	0,
+	0);
+
+	if(map == MAP_FAILED){
+		fprintf(stderr, "malloc failed\n");
 		return NULL;
 	}
 
-		region->status = true;
-		region->level = 7;
-		region->next = NULL;
-		region->prev = NULL;
+	map->status = true;
+	map->level = 7;
+	map->next = NULL;
+	map->prev = NULL;
 
+	return map;
 
-	return region;
 }
 
 struct head *buddy(struct head* block){
 
-
-	//top most block
-	if(block->level == 6)
-	{
+	if(block->level == 7){
+		fprintf(stderr, "Does not have buddy\n");
 		return NULL;
 	}
-
-	//is the beginning
-	if(block->prev == NULL)
-	{
-		if(block->next != NULL)
-		{
-			return block->next;
-		}
-		else
-		{
-			perror(" \n Couldn't find buddy \n");
-		}
-	}
-	else
-	{
+	else if(block->next == NULL){
 		return block->prev;
 	}
+	else{
+		return block->next;
+	}
+
+
 
 }
 
 struct head *split(struct head* block){
 
-	if(block->level != 0)
-	{
-		block->level = block->level -1;
-		block->next = block + (int)(pow(2,block->level _ MIN)/2);
-
-		//point to next and point back
-
-		return block;
-	}
-	else
-	{
-		perror("You can't split");
+	if(block->level == 0){
+		fprintf(stderr, "Cannot split\n");
 		return block;
 	}
 
-}
+	//struct head *temp = block->next;
 
-struct head *merge(struct head* h1,struct head* h2)
-{
-	struct head* merged_block = primary(h1);
+	block->level = block->level - 1;
+	block->next = block + (int)(pow(2, block->level+MINIMUM)/2);
 
-	if(merged_block == h1)
-	{
-		merged_block->level  = h1->level +1;
+	// struct head *next = block->next;
+	// next->next = temp;
+	// next->prev = block;
 
-		//merger happens here
-
-	}
-	else
-	{
-		merged_block = primary(h2);
-	}
+	return block;
 
 
-
-	return merged_block;
 }
 
 struct head *primary(struct head* block){
 
-	if(block->prev == NULL)
-	{
+	if(block->prev != NULL){
+		return block->prev;
+	}else{
 		return block;
 	}
-	else
-	{
-		return block->prev;
+
+}
+
+struct head *merge(struct head* blockA, struct head* blockB){
+
+	if(primary(blockA) == blockA){
+		blockA->next = blockB->next;
+		blockA->level = blockA->level + 1;
+		return blockA;
+	}
+	else{
+		blockB->next = blockA->next;
+		blockB->level = blockB->level + 1;
+		return blockB;
 	}
 
 }
 
 void *hide(struct head* block){
 
+	return (block + sizeof(struct head));
+
 }
+
 
 struct head *magic(void *memory){
 
+	return (struct head*)(memory - sizeof(struct head));
+
 }
+
 
 int level(int req){
 
-	return (int)round(log2(req)) - MIN;
+	if(req > 4096){
+		fprintf(stderr, "Request too big \n");
+		return 1;
+	}
+	else{
+		double x = round(log2(req));
+		int level = (int) x;
+		return level - MINIMUM;
+	}
 }
 
 void dispblocklevel(struct head* block){
@@ -152,17 +149,4 @@ void blockinfo(struct head* block){
 	printf("start of block in memory: %p\n", block);
 	printf("size of block in memory: %ld in bytes\n",sizeof(struct head));
 	printf("===================================================================\n");
-}
-
-// Function to calculate x raised to the power y */
-int power(int x,int y)
-{
-	int ans =1;
-
-  for(int i = 1;i <= y;i++)
-	{
-			ans = ans*2;
-	}
-
-	return ans;
 }
--- buddy.h
+++ buddy.h
@@ -10,9 +10,11 @@ struct head *new();
 struct head *buddy(struct head*);
 struct head *split(struct head*);
 struct head *primary(struct head*);
+struct head *merge(struct head*, struct head*);
 void *hide(struct head*);
 struct head *magic(void*);
 int level(int);
+
 void dispblocklevel(struct head*);
 void dispblockstatus(struct head*);
 void blockinfo(struct head*);
